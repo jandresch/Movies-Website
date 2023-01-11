41d87c3e1261a327922cda34ead0ad3eda28023e
Javier <jandresch017@gmail.com>
2023-01-10 23:31:14 -0600

    primeras interacciones de los botones

--- src/navigation.js
+++ src/navigation.js
@@ -1,11 +1,48 @@
-window.addEventListener('load', navigate);
+window.addEventListener('DOMContentLoaded', navigate);
+window.addEventListener('hashchange', navigate);
 
 function navigate(){
-    console.log('si me ejecuto perro')
     if(location.hash.startsWith('#home')){
-        console.log('Estas en el home')
+        homeLocation();
     }else if(location.hash.startsWith('#trending')){
-        console.log('Estas en el home')
+        console.log('Estas los trends');
+        trendingSection();
+    }else if(location.hash.startsWith('#movies')){
+        console.log('Estas en las peliculas');
+        moviesSection();
+    }else if(location.hash.startsWith('#series')){
+        console.log('Estas en las series');
+        seriesSection();
+    }else{
+        console.log('Estas en el home');
+        // location.hash = 'home'
     }
 }
 
+function homeLocation(){
+    console.log('Estas en el home');
+    trendingMoviesSection.classList.remove('inactive');
+    generalMoviesSection.classList.remove('inactive');
+    generalSeriesSection.classList.remove('inactive');
+    moreTrendsBtn.onclick = () => {location.hash = 'trending'};
+    moreMoviesBtn.onclick = () => {location.hash = 'movies'};
+    moreSeriesBtn.onclick = () => {location.hash = 'series'};
+}
+
+function trendingSection(){
+    trendingMoviesSection.classList.add('inactive');
+    generalMoviesSection.classList.add('inactive');
+    generalSeriesSection.classList.add('inactive');
+}
+
+function moviesSection(){
+    trendingMoviesSection.classList.add('inactive');
+    generalMoviesSection.classList.remove('inactive');
+    generalSeriesSection.classList.add('inactive');
+}
+
+function seriesSection(){
+    trendingMoviesSection.classList.add('inactive');
+    generalMoviesSection.classList.add('inactive');
+    generalSeriesSection.classList.remove('inactive');   
+}
